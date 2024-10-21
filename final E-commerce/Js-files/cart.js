19927655bad7834500f0fba1d6db2f9de41ffea6
Aniketbhandari18 <166018090+Aniketbhandari18@users.noreply.github.com>
2024-10-21 21:34:56 +0530

    Add files via upload

--- final E-commerce/Js-files/cart.js
+++ final E-commerce/Js-files/cart.js
@@ -1,3 +1,10 @@
+window.addEventListener('pageshow', function(event) {
+    if (event.persisted) {  // Checks if the page was loaded from cache
+        window.location.reload();  // Forces a reload to get fresh data
+    }
+});
+
+
 export let cart = JSON.parse(localStorage.getItem('cart'));
 
 if (!cart) cart = [];
